3aacce049afd7d17f7ed7565aec96c849ad3009d
edday57 <pandora999>
2016-09-05 23:50:40 +0100

    Adding constraints and custom UI for user profile
    Need to increase scale of background image

--- CookBook/ProfileHeaderView.swift
+++ CookBook/ProfileHeaderView.swift
@@ -8,9 +8,12 @@
 
 import UIKit
 import Parse
+import CoreImage
 
 class ProfileHeaderView: UICollectionReusableView {
         
+    @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var container: UIView!
     @IBOutlet weak var avaImg: UIImageView!
     @IBOutlet weak var fullnameLbl: UILabel!
     @IBOutlet weak var usernameLbl: UILabel!
@@ -25,6 +28,59 @@ class ProfileHeaderView: UICollectionReusableView {
     @IBOutlet weak var followingTitle: UILabel!
     
     @IBOutlet weak var button: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        //allignment
+        let width = UIScreen.main.bounds.width
+        
+        //avaImg.frame = CGRect(x: 12, y: 12, width: 112, height: 112)
+        postTitle.frame = CGRect(x: width / 16.3, y: 140, width: width / 4, height: 17)
+        posts.frame = CGRect(x: width / 16.3, y: 160, width: width / 4, height: 30)
+        
+        followersTitle.frame = CGRect(x: width / 2.4, y: 140, width: width / 4, height: 17)
+        followers.frame = CGRect(x: width / 2.4, y: 160, width: width / 4, height: 30)
+        
+        followingTitle.frame = CGRect(x: width / 1.37, y: 140, width: width / 4, height: 17)
+        following.frame = CGRect(x: width / 1.37, y: 160, width: width / 4, height: 30)
+        
+        avaImg.layer.cornerRadius = 56
+        avaImg.clipsToBounds = true
+        
+        avaImg.layer.borderColor = UIColor.white.cgColor
+        avaImg.layer.borderWidth = 2
+        
+        container.backgroundColor = UIColor.clear
+        container.layer.shadowColor = UIColor.black.cgColor
+        container.layer.shadowRadius = 3
+        container.layer.shadowOffset = CGSize(width: 0, height: 1)
+        container.layer.shadowOpacity = 1
+        
+     //   let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.extraLight)
+    //    let blurView = UIVisualEffectView(effect: blurEffect)
+      //  blurView.frame = backgroundImage.bounds
+       // blurView.alpha = 0.7
+      //  applyBlurEffect(image: backgroundImage.image!)
+        let context = CIContext(options: nil)
+        let imageToBlur = CIImage(image:backgroundImage.image!)
+        let blurfilter = CIFilter(name: "CIGaussianBlur")
+        blurfilter!.setValue(imageToBlur, forKey: "inputImage")
+        blurfilter!.setValue(1.5, forKey: "inputRadius")
+        let resultImage = blurfilter!.value(forKey: "outputImage") as! CIImage
+        let cgImage = context.createCGImage(resultImage, from: resultImage.extent)
+        let blurredImage = UIImage(cgImage: cgImage!)
+        backgroundImage.image = blurredImage
+        
+        
+    }
+
+
+    
+    
+    
+    
+    
     
     @IBAction func followButtonClicked(_ sender: AnyObject) {
         let title = button.title(for: UIControlState.normal)!
